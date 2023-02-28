d4b450b24640aa91e04d28acdaf0791dcce97192
Oluwapelumi Oyenuga <oluwapelumioyenuga@gmail.com>
2023-02-28 12:09:02 +0100

    delete recipe endpoint

--- main.go
+++ main.go
@@ -24,6 +24,30 @@ func init() {
 	recipes = make([]Recipe, 0)
 }
 
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	index := -1
+
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].Id == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+
+	recipes = append(recipes[:index], recipes[index+1:]...)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe deleted successfully",
+	})
+}
+
 func UpdateRecipeHandler(c *gin.Context) {
 	id := c.Param("id")
 
@@ -84,5 +108,6 @@ func main() {
 	router.POST("/recipe", NewRecipeHandler)
 	router.GET("/recipe", ListAllRecipeHandler)
 	router.PUT("/recipe/:id", UpdateRecipeHandler)
+	router.DELETE("/recipe/:id", DeleteRecipeHandler)
 	router.Run()
 }
